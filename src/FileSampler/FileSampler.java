816c619c8f02743181d4799a45e810f970c86346
Vrethur91 <bene.vrethur@gmail.com>
2015-06-11 09:09:04 +0200

    Changed options to enum classes. Added new filter.

--- src/FileSampler/FileSampler.java
+++ src/FileSampler/FileSampler.java
@@ -1,4 +1,4 @@
-
+package FileSampler;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -12,62 +12,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import limes_qgram.LimesQgram;
 
 /**
  *
  * @author Bene
  */
-public class LimesQgram {
+public class FileSampler {
 
-    private HashSet<Integer> indexSet;
-    private double threshold = 0.6;
-    private int q = 3;
-
-    public static void main(String[] args) {
-        LimesQgram qgram = new LimesQgram();
-
-        qgram.setThreshold(0.6);
-        qgram.setQ(3);
-        qgram.prepareData();
-    }
-
-    public LimesQgram() {
-        indexSet = new HashSet<>();
-    }
-
-    public void setThreshold(double threshold) {
-        this.threshold = threshold;
-    }
-
-    public void setQ(int q) {
-        this.q = q;
-    }
-
-    public void prepareData(){
-        /*
-        ArrayList<String> list = importData();
-        ArrayList<String> list1 = getSample(list, 10000);
-        ArrayList<String> list2 = getSample(list, 5000);
-        
-        saveList("./data/sampleA.txt",list1);
-        saveList("./data/sampleB.txt",list2);
-        */
-        
-        ArrayList<String> list1 = loadList("./data/sampleA.txt");
-        ArrayList<String> list2 = loadList("./data/sampleB.txt");
-        
-        
-        FilterTestInterface basicNormal = new FilterTestBasicNormal();
-        basicNormal.runTest(list1, list2, threshold, q);
-        /*
-        FilterTestInterface basicFilter = new FilterTestBasicFilter();
-        basicFilter.runTest(list1, list2, threshold, q);
-                */
-    }
-
-    private ArrayList<String> getSample(ArrayList<String> list, int size) {
+    public ArrayList<String> getSample(ArrayList<String> list, int size) {
         ArrayList<String> sampleList = new ArrayList<>();
-
+        HashSet<Integer> indexSet = new HashSet<>();
         while (sampleList.size() < size) {
             int randomIndex = (int) (Math.random() * list.size());
             if (!indexSet.contains(randomIndex)) {
@@ -79,27 +34,13 @@ public class LimesQgram {
         return sampleList;
     }
 
-    private ArrayList<String> loadList(String filepath){
-        ArrayList<String> list = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                list.add(line);
-            }
-        } catch (Exception ex) {
-            Logger.getLogger(LimesQgram.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return list;
-    }
-    
-    private void saveList(String filepath, ArrayList<String> list){
-        try{
+    public void saveList(String filepath, ArrayList<String> list) {
+        try {
             File file = new File(filepath);
             file.createNewFile();
             BufferedWriter bWriter = new BufferedWriter(new FileWriter(file));
             bWriter.write("");
-            for(String string:list){
+            for (String string : list) {
                 bWriter.append(string + "\n");
             }
             bWriter.flush();
@@ -108,8 +49,8 @@ public class LimesQgram {
             Logger.getLogger(LimesQgram.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private ArrayList<String> importData() {
+
+    public ArrayList<String> importData() {
         ArrayList<String> list = new ArrayList<>();
         String filepath = "./data/labels.txt";
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"))) {
@@ -124,7 +65,7 @@ public class LimesQgram {
         return list;
     }
 
-    private void convertData() {
+    public void convertData() {
         String filepath = "./data/labels_en.ttl";
         File file = new File("./data/labels.txt");
 
---
+++ src/limes_qgram/LimesQgram.java
@@ -0,0 +1,364 @@
+package limes_qgram;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Bene
+ */
+public class LimesQgram {
+
+    private HashMap<Integer, ArrayList<String>> sortByLengthList1;
+    private HashMap<Integer, ArrayList<String>> sortByLengthList2;
+    private HashMap<String, HashSet<String>> sortByQGram;
+    private Validation validation;
+    private Tokenizer tokenizer;
+    private Formula formula;
+    private Bounds bounds;
+    private double threshold;
+    private int q;
+
+    public static void main(String[] args) {
+        LimesQgram qgram = new LimesQgram();
+        qgram.setTokenizer(Tokenizer.ENDS);
+        qgram.setValidation(Validation.ENDS);
+        qgram.setBounds(Bounds.ENDS);
+
+        qgram.runBenchmark();
+    }
+
+    public LimesQgram() {
+        this.bounds = Bounds.NO_BOUNDS;
+        this.formula = Formula.JACCARD;
+        this.tokenizer = Tokenizer.BASIC;
+        this.validation = Validation.ALWAYS_TRUE;
+        this.threshold = 0.6;
+        this.q = 3;
+    }
+
+    public void setThreshold(double threshold) {
+        this.threshold = threshold;
+    }
+
+    public void setQ(int q) {
+        this.q = q;
+    }
+
+    public void setTokenizer(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+    }
+
+    public void setValidation(Validation validation) {
+        this.validation = validation;
+    }
+
+    public void setBounds(Bounds bounds) {
+        this.bounds = bounds;
+    }
+
+    public void runBenchmark() {
+        ArrayList<String> list1 = loadList("./data/sampleA.txt");
+        ArrayList<String> list2 = loadList("./data/sampleB.txt");
+
+        long totalTime = 0;
+
+        for (int i = 0; i < 10; i++) {
+            long timeStart1 = System.currentTimeMillis();
+            int hit = 0;
+            if (bounds.equals(Bounds.NO_BOUNDS)) {
+                for (String string1 : list1) {
+                    for (String string2 : list2) {
+                        if (validation.hasValidLengthDiff(string1, string2, q, threshold)) {
+                            double sim = compare(string1, string2);
+                            if (sim >= threshold) {
+                                hit++;
+                            }
+                        }
+                    }
+                }
+            } else {
+                sortByLength(list1, list2);
+                for (Entry<Integer, ArrayList<String>> entry : sortByLengthList1.entrySet()) {
+                    int size = entry.getKey();
+                    Integer[] currentBounds = bounds.getBounds(size, q, threshold);
+                    int min = currentBounds[0];
+                    int max = currentBounds[1];
+                    for (String string1 : entry.getValue()) {
+                        for (int currentLength = min; currentLength <= max; currentLength++) {
+                            if (sortByLengthList2.containsKey(currentLength)) {
+                                for (String string2 : sortByLengthList2.get(currentLength)) {
+                                    if (validation.hasValidLengthDiff(string1, string2, q, threshold)) {
+                                        double sim = compare(string1, string2);
+                                        if (sim >= threshold) {
+                                            hit++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            long timeEnd1 = System.currentTimeMillis();
+            totalTime += (timeEnd1 - timeStart1);
+        }
+
+        double avgTime = totalTime / 10.0;
+        System.out.println("Zeit: " + avgTime);
+
+    }
+
+    private double compare(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return 1.0;
+        }
+
+        Set<String> q1 = tokenizer.qgrams(s1, q);
+        Set<String> q2 = tokenizer.qgrams(s2, q);
+
+        if (q1.isEmpty() || q2.isEmpty()) {
+            return 0.0; // division will fail
+        }
+
+        int common = 0;
+        for (String gram : q1) {
+            if (q2.contains(gram)) {
+                common++;
+            }
+        }
+
+        return formula.compute(common, q1, q2);
+    }
+
+    private void sortByQGram(ArrayList<String> list1, ArrayList<String> list2) {
+        sortByQGram = new HashMap<>();
+        for (String string1 : list1) {
+            Set<String> gramSet = tokenizer.qgrams(string1, q);
+            if (gramSet.isEmpty()) {
+                continue;
+            } else {
+                for (String gram : gramSet) {
+                    if (sortByQGram.containsKey(gram)) {
+                        sortByQGram.get(gram).add(string1);
+                    } else {
+                        HashSet<String> labelSet = new HashSet<>();
+                        labelSet.add(string1);
+                        sortByQGram.put(gram, labelSet);
+                    }
+                }
+            }
+        }
+
+        for (String string1 : list2) {
+            Set<String> gramSet = tokenizer.qgrams(string1, q);
+            if (gramSet.isEmpty()) {
+                continue;
+            } else {
+                for (String gram : gramSet) {
+                    if (sortByQGram.containsKey(gram)) {
+                        sortByQGram.get(gram).add(string1);
+                    } else {
+                        HashSet<String> labelSet = new HashSet<>();
+                        labelSet.add(string1);
+                        sortByQGram.put(gram, labelSet);
+                    }
+                }
+            }
+        }
+    }
+
+    private void sortByLength(ArrayList<String> list1, ArrayList<String> list2) {
+        sortByLengthList1 = new HashMap<>();
+        sortByLengthList2 = new HashMap<>();
+        for (String string1 : list1) {
+            if (sortByLengthList1.containsKey(string1.length())) {
+                sortByLengthList1.get(string1.length()).add(string1);
+            } else {
+                ArrayList<String> labelList = new ArrayList<>();
+                labelList.add(string1);
+                sortByLengthList1.put(string1.length(), labelList);
+            }
+        }
+
+        for (String string2 : list2) {
+            if (sortByLengthList2.containsKey(string2.length())) {
+                sortByLengthList2.get(string2.length()).add(string2);
+            } else {
+                ArrayList<String> labelList = new ArrayList<>();
+                labelList.add(string2);
+                sortByLengthList2.put(string2.length(), labelList);
+            }
+        }
+    }
+
+    private ArrayList<String> loadList(String filepath) {
+        ArrayList<String> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(LimesQgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return list;
+    }
+
+    private static String pad(String s, int q, boolean front) {
+        StringBuffer buf = new StringBuffer(q);
+        if (!front) {
+            buf.append(s);
+        }
+        for (int ix = 0; ix < q - s.length(); ix++) {
+            buf.append('.');
+        }
+        if (front) {
+            buf.append(s);
+        }
+        return buf.toString();
+    }
+
+    public enum Formula {
+
+        JACCARD {
+                    public double compute(int common, Set<String> q1, Set<String> q2) {
+                        return (double) common / (double) (q1.size() + q2.size() - common);
+                    }
+                };
+
+        public double compute(int common, Set<String> q1, Set<String> q2) {
+            System.out.println("Unknown formula: " + this);
+            return 0;
+        }
+    }
+
+    public enum Tokenizer {
+
+        BASIC {
+                    public Set<String> qgrams(String s, int q) {
+                        Set<String> grams = new HashSet();
+                        for (int ix = 0; ix < s.length() - q + 1; ix++) {
+                            grams.add(s.substring(ix, ix + q));
+                        }
+                        return grams;
+                    }
+                },
+        ENDS {
+                    public Set<String> qgrams(String s, int q) {
+                        Set<String> grams = new HashSet();
+                        for (int ix = 1; (ix < q) && (ix < s.length()); ix++) {
+                            grams.add(pad(s.substring(0, ix), q, true));
+                        }
+                        for (int ix = 0; ix < s.length() - q + 1; ix++) {
+                            grams.add(s.substring(ix, ix + q));
+                        }
+                        for (int ix = 1; (ix < q) && (ix < s.length()); ix++) {
+                            grams.add(pad(s.substring(s.length() - ix), q, false));
+                        }
+
+                        return grams;
+                    }
+                };
+
+        public Set<String> qgrams(String s, int q) {
+            System.out.println("Unknown tokenizer: " + this);
+            return null;
+        }
+    }
+
+    public enum Validation {
+
+        ALWAYS_TRUE {
+                    public boolean hasValidLengthDiff(String string1, String string2, int q, double threshold) {
+                        return true;
+                    }
+                },
+        BASIC {
+                    public boolean hasValidLengthDiff(String string1, String string2, int q, double threshold) {
+                        double size1 = string1.length();
+                        double size2 = string2.length();
+
+                        if (size1 - (q - 1) <= 0 || size2 - (q - 1) <= 0) {
+                            return false;
+                        }
+
+                        if ((Math.min(size1, size2) - (q - 1)) / (Math.max(size1, size2) - (q - 1)) >= threshold) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                },
+        ENDS {
+                    public boolean hasValidLengthDiff(String string1, String string2, int q, double threshold) {
+                        double size1 = string1.length();
+                        double size2 = string2.length();
+
+                        if ((Math.min(size1, size2) + (q - 1)) / (Math.max(size1, size2) + (q - 1)) >= threshold) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+
+                };
+
+        public boolean hasValidLengthDiff(String string1, String string2, int q, double threshold) {
+            System.out.println("Unknown validation option: " + this);
+            return false;
+        }
+
+    }
+
+    public enum Bounds {
+
+        NO_BOUNDS {
+                    public Integer[] getBounds(int size, int q, double threshold) {
+                        return null;
+                    }
+                },
+        BASIC {
+                    public Integer[] getBounds(int size, int q, double threshold) {
+                        Integer[] bounds = new Integer[2];
+
+                        if (size - (q - 1) <= 0) {
+                            bounds[0] = 1;
+                        } else {
+                            bounds[0] = (int) (threshold * (size - (q - 1)) + (q - 1)) + 1;
+                        }
+
+                        bounds[1] = (int) ((size - (q - 1)) / threshold + (q - 1));
+
+                        return bounds;
+                    }
+                },
+        ENDS {
+                    public Integer[] getBounds(int size, int q, double threshold) {
+                        Integer[] bounds = new Integer[2];
+
+                        bounds[0] = (int) (threshold * (size + (q - 1)) - (q - 1)) + 1;
+
+                        bounds[1] = (int) ((size + (q - 1)) / threshold - (q - 1));
+
+                        return bounds;
+                    }
+                };
+
+        public Integer[] getBounds(int size, int q, double threshold) {
+            System.out.println("Unknown validation option");
+            return null;
+        }
+    }
+}
